f9bdbebb2b3c441898f5c37ce128dc6d9e2ff737
gaituo <gaituoshinra@outlook.com>
2021-03-14 18:07:23 +0000

    Bringup to wave

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/potato_sanders.mk
+	$(LOCAL_DIR)/wave_sanders.mk
 
 COMMON_LUNCH_CHOICES := \
-	potato_sanders-userdebug
+	wave_sanders-userdebug
